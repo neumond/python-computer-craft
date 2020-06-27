5b0ac9638e386da15a0f3f1790a2b110e84c12fb
neumond <knifeslaughter@gmail.com>
2020-06-27 18:44:50 +0300

    Rewrite to websockets

--- computercraft/server.py
+++ computercraft/server.py
@@ -1,7 +1,7 @@
 import asyncio
 import json
 import string
-from aiohttp import web
+from aiohttp import web, WSMsgType
 from traceback import print_exc
 from os.path import join, dirname, abspath
 from importlib import import_module
@@ -43,12 +43,6 @@ def base36(n):
     return r[::-1]
 
 
-async def lua_json(request):
-    body = await request.text()
-    body = body.replace('\\\n', '\\n')
-    return json.loads(body)
-
-
 class CCAPI(RootAPIMixin):
     def __init__(self, nid, program, cleanup_callback):
         self._id = nid
@@ -57,6 +51,8 @@ class CCAPI(RootAPIMixin):
         self._result_locks = {}
         self._result_values = {}
         self._result_queues = {}
+        self._event_to_tids = {}
+        self._tid_to_event = {}
 
         self.colors = ColorsAPI
         self.commands = CommandsAPI(self)
@@ -78,24 +74,21 @@ class CCAPI(RootAPIMixin):
         self.window = WindowAPI(self)  # TODO: unimplemented
 
         async def prog_wrap():
-            cancel = False
             try:
                 await program(self)
             except asyncio.CancelledError:
                 print('program {} cancelled'.format(self._id))
                 print_exc()
-                cancel = True
             except Exception as e:
                 print('program {} crashed: {} {}'.format(self._id, type(e), e))
                 print_exc()
             else:
                 print('program {} finished'.format(self._id))
             finally:
-                if not cancel:
-                    await self._cmd.put('END')
+                await self._cmd.put({'action': 'close'})
                 cleanup_callback()
 
-        self._task = asyncio.ensure_future(prog_wrap())
+        self._task = asyncio.create_task(prog_wrap())
 
     def _new_task_id(self):
         task_id = base36(self._task_autoid)
@@ -105,7 +98,11 @@ class CCAPI(RootAPIMixin):
     async def _send_cmd(self, lua):
         task_id = self._new_task_id()
         self._result_locks[task_id] = asyncio.Event()
-        await self._cmd.put('TASK;' + task_id + ';' + lua)
+        await self._cmd.put({
+            'action': 'task',
+            'task_id': task_id,
+            'code': lua,
+        })
         await self._result_locks[task_id].wait()
         del self._result_locks[task_id]
         result = self._result_values.pop(task_id)
@@ -117,77 +114,88 @@ class CCAPI(RootAPIMixin):
     async def _start_queue(self, event):
         task_id = self._new_task_id()
         self._result_queues[task_id] = asyncio.Queue()
-        await self._cmd.put('STARTQUEUE;' + task_id + ';' + event)
+        es = self._event_to_tids.setdefault(event, set())
+        if not es:
+            await self._cmd.put({
+                'action': 'sub',
+                'event': event,
+            })
+        es.add(task_id)
+        self._tid_to_event[task_id] = event
         return self._result_queues[task_id], task_id
 
     async def _stop_queue(self, task_id):
-        await self._cmd.put('STOPQUEUE;' + task_id)
+        event = self._tid_to_event[task_id]
         del self._result_queues[task_id]
-
-
-logging_config = '''
-version: 1
-disable_existing_loggers: false
-root:
-  level: ERROR
-  handlers:
-  - console
-handlers:
-  console:
-    level: INFO
-    class: logging.StreamHandler
-    formatter: verbose
-loggers:
-  aiohttp.access:
-    level: INFO
-    handlers: []
-    propagate: true
-formatters:
-  verbose:
-    format: '%(message)s'
-'''
-
-
-def enable_request_logging():
-    import logging.config
-    import yaml
-    logging.config.dictConfig(yaml.load(logging_config))
+        del self._tid_to_event[task_id]
+        self._event_to_tids[event].discard(task_id)
+        if not self._event_to_tids[event]:
+            await self._cmd.put({
+                'action': 'unsub',
+                'event': event,
+            })
 
 
 class CCApplication(web.Application):
-    async def start(self, request):
-        tid = int(request.match_info['turtle'])
-        if tid in self['exchange']:
-            # terminate old program
-            self['exchange'][tid]._task.cancel()
-        module = import_module(self['source_module'])
-        program = getattr(module, request.match_info['program'])
+    @staticmethod
+    async def _sender(ws, api):
+        while not ws.closed:
+            cmd = await api._cmd.get()
+            print(f'_sender: {cmd}')
+            if not ws.closed:
+                await ws.send_json(cmd)
+            if cmd['action'] == 'close':
+                break
 
-        def cleanup_callback():
-            del self['exchange'][tid]
+    @staticmethod
+    async def _json_messages(ws):
+        async for msg in ws:
+            print('ws received', msg)
+            if msg.type != WSMsgType.TEXT:
+                continue
+            # print('ws received', msg.data)
+            yield json.loads(msg.data.replace('\\\n', '\\n'))
 
-        self['exchange'][tid] = CCAPI(tid, program, cleanup_callback)
-        return web.Response(text='')
+    async def _launch_program(self, ws):
+        async for msg in self._json_messages(ws):
+            if msg['action'] != 'run':
+                await ws.send_json({
+                    'action': 'close',
+                    'error': 'protocol error',
+                })
+                return None
+            module = import_module(self['source_module'])
+            program = getattr(module, msg['args'][0], None)
+            if program is None:
+                await ws.send_json({
+                    'action': 'close',
+                    'error': "program doesn't exist",
+                })
+                return None
+            return CCAPI(msg['computer'], program, lambda: None)
 
-    async def gettask(self, request):
-        api = self['exchange'].get(int(request.match_info['turtle']))
-        if api is None:
-            return web.Response(text='END')
-        return web.Response(text=await api._cmd.get())
+    async def ws(self, request):
+        ws = web.WebSocketResponse()
+        await ws.prepare(request)
 
-    async def taskresult(self, request):
-        api = self['exchange'].get(int(request.match_info['turtle']))
+        api = await self._launch_program(ws)
         if api is not None:
-            tid = request.match_info['task_id']
-            if tid in api._result_locks:
-                # it's a TASK
-                api._result_values[tid] = await lua_json(request)
-                api._result_locks[tid].set()
-            elif tid in api._result_queues:
-                # it's a QUEUE
-                await api._result_queues[tid].put(await lua_json(request))
-            # otherwise just ignore
-        return web.Response(text='')
+            asyncio.create_task(self._sender(ws, api))
+            async for msg in self._json_messages(ws):
+                if msg['action'] == 'event':
+                    for task_id in self._event_to_tids.get(msg['event'], ()):
+                        await api._result_queues[task_id].put(msg['params'])
+                elif msg['action'] == 'task_result':
+                    api._result_values[msg['task_id']] = msg['result']
+                    api._result_locks[msg['task_id']].set()
+                else:
+                    await ws.send_json({
+                        'action': 'close',
+                        'error': 'protocol error',
+                    })
+                    break
+
+        return ws
 
     @staticmethod
     def backdoor(request):
@@ -199,7 +207,7 @@ class CCApplication(web.Application):
             h += ':{}'.format(request.app['port'])
         fcont = fcont.replace(
             "local url = 'http://127.0.0.1:4343/'",
-            "local url = '{}://{}/'".format(request.scheme, h)
+            "local url = '{}://{}/'".format('ws', h)
         )
         return web.Response(text=fcont)
 
@@ -207,14 +215,10 @@ class CCApplication(web.Application):
         self['source_module'] = source_module
         self['exchange'] = {}
         self.router.add_get('/', self.backdoor)
-        self.router.add_post('/start/{turtle}/{program}/', self.start)
-        self.router.add_post('/gettask/{turtle}/', self.gettask)
-        self.router.add_post('/taskresult/{turtle}/{task_id}/', self.taskresult)
+        self.router.add_get('/ws/', self.ws)
 
 
 def main():
-    # enable_request_logging()
-
     parser = argparse.ArgumentParser()
     parser.add_argument('module', help='Module used as source for programs')
     parser.add_argument('--host')
