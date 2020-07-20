daded5111fd8443c2e175d787f0a7e4d618d5a4e
neumond <knifeslaughter@gmail.com>
2020-07-21 02:50:33 +0300

    Cleanup

--- computercraft/subapis/gps.py
+++ computercraft/subapis/gps.py
@@ -1,11 +1,9 @@
 from typing import Tuple, Optional
 
 from ..lua import LuaNum
-from ..rproc import tuple3_number, fact_option
 from ..sess import eval_lua_method_factory
 
 
-option_tuple3_number = fact_option(tuple3_number)
 method = eval_lua_method_factory('gps.')
 
 
